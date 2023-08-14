fc78ee174a6165993ae7925554e772e796cfe749
Claudio Henrique <simonelliclaudiohenrique@gmail.com>
2023-08-13 21:45:53 -0400

    fix: remove arquivo desnecessario

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { CreateUserDTO } from "src/users/dto/create-user.dto";
+import { Public } from "../decorators/public.decorator";
 
 @Controller("auth")
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @Post("login")
   signIn(@Body() signInDto: CreateUserDTO) {
     return this.authService.signIn(signInDto.email, signInDto.password);
